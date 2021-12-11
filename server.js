51618ffe88433b4a293bfeb7f725b1a62045be7d
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-11 10:36:00 -0500

    pls

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Methods",
   //     "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "https://sharp-northcutt-1bdda8.netlify.app"),
+  res.header("Access-Control-Allow-Origin", "*"),
   res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS"),
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   next();
