e3e56a6dedc504bebb42cccea17920cb9e679a1d
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-11 10:23:47 -0500

    pls lord

--- consts.js
+++ consts.js
@@ -1,6 +1,4 @@
-//const MONGODB_URL = 'mongodb://localhost:27017/tuiter';
-const MONGODB_URL = 'mongodb+srv://db_design:LQRzHX0nEXM8bONY@cluster0.ym2cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+const MONGODB_URL = 'mongodb+srv://root:web-dev@pokedex-db.ccjht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 module.exports = {
   MONGODB_URL
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-      "https://serene-wilson-429f98.netlify.app");
+      "http://localhost:3000");
   res.header("Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
@@ -12,6 +12,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
+
+// http://localhost:3000
+// https://sharp-northcutt-1bdda8.netlify.app
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
