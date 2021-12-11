550817035e4cc38c16858d292c3499962d770a6c
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-11 10:32:30 -0500

    lmao

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin",
-      "https://sharp-northcutt-1bdda8.netlify.app");
-  res.header("Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS");
+  // res.header("Access-Control-Allow-Origin",
+  //     "https://sharp-northcutt-1bdda8.netlify.app");
+  // res.header("Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Methods",
+  //     "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "https://sharp-northcutt-1bdda8.netlify.app"),
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS"),
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   next();
 });
 
