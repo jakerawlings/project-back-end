e33a84f5a9bc312b2c81eeda25f1ef585cea3729
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-13 20:02:06 -0500

    getting there

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-      "https://sharp-northcutt-1bdda8.netlify.app");
+      "http://localhost:3000");
   res.header("Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
@@ -13,9 +13,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// https://sharp-northcutt-1bdda8.netlify.app
-// http://localhost:3000
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,7 +20,9 @@ app.use(bodyParser.json());
 const session = require('express-session')
 app.use(session({
   secret: 'keyboard cat',
-  cookie: {}
+  cookie: {},
+  resave: true,
+  saveUninitialized: true
 }));
 
 const mongoose = require('mongoose');
--- users/user-schema.js
+++ users/user-schema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+const {Schema} = require("mongoose");
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
   email: String,
-  firstName: String,
-  lastName: String,
+  favorites: {type: [Schema.Types.Mixed], defaultValue: []}
 }, {collection: 'users'});
 module.exports = userSchema;
